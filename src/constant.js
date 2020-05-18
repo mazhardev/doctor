147bb4b695382758de2e917ddccc6c95374b1c5a
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2020-05-18 21:49:38 +0500

    make it best

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = "https://mazhar-api.herokuapp.com";
+export const API_ENDPOINT = "https://boiling-refuge-94001.herokuapp.com";
 
 export const IMAGE_ENDPOINT = API_ENDPOINT;
 
